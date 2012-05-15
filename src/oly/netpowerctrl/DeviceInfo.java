e8273a0c84a203b955b0ef3788337398d7f153d7
oly <oly@nittka.com>
2012-05-16 01:42:14 +0200

    DeviceInfo: added ports and user/pwd

--- src/oly/netpowerctrl/DeviceInfo.java
+++ src/oly/netpowerctrl/DeviceInfo.java
@@ -7,6 +7,12 @@ public class DeviceInfo {
 
 	public String DeviceName; // name of the device as reported by UDP or configured by the user
 	public String HostName;   // the hostname or ip address used to reach the device
+
+	public String UserName;
+	public String Password;
+	
+	public int SendPort = 1075;
+	public int RecvPort = 1077;
 	
 	public List<OutletInfo> Outlets;
 	
