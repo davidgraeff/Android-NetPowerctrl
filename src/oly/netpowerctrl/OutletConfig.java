251f21ee43dfff8f959988cd28c92f6c2d4a105a
oly <oly@nittka.com>
2012-07-02 00:49:14 +0200

    implemented outlet_config as listview (currently read-only)

--- src/oly/netpowerctrl/OutletConfig.java
+++ src/oly/netpowerctrl/OutletConfig.java
@@ -1,92 +1,87 @@
 package oly.netpowerctrl;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class OutletConfig extends Activity {
 
-	TableLayout tlyoOutlets;
-	int RowCounter;
+	ListView lvOutlets;
+	OutletConfigAdapter adapter;
+	DeviceInfo device_info;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.outlet_config);
-		tlyoOutlets = (TableLayout)findViewById(R.id.tlyoOutlets);
-        DeviceInfo device_info = (DeviceInfo) getIntent().getExtras().get("device_info");
+		lvOutlets = (ListView)findViewById(R.id.lvOutlets);
+        device_info = (DeviceInfo) getIntent().getExtras().get("device_info");
 
-        ((TextView)findViewById(R.id.tvTitle)).setText(device_info.DeviceName);
-        
-        RowCounter = 0;
-        for (OutletInfo oi: device_info.Outlets)
-        	AddRow(oi.Description, String.format("%d",oi.OutletNumber));
-                
-        findViewById(R.id.btnAddOutlet).setOnClickListener(onBtnAddClicked);
+        adapter = new OutletConfigAdapter(this, device_info.Outlets);
+        lvOutlets.setAdapter(adapter);
+
+        ImageButton btnAdd = (ImageButton)findViewById(R.id.btnAddOutlet);
+        btnAdd.setOnClickListener(onBtnAddClicked);
 	}
 
-	public void AddRow(String name, String outletno) {
-    	RowCounter++;
-    	
-    	EditText edtName = new EditText(this);
-    	edtName.setEms(0);
-    	edtName.setTextAppearance(this, android.R.attr.textAppearanceSmall);
-    	edtName.setText(name);
-    	edtName.setTag(new Integer(RowCounter));
-    	
-    	EditText edtOutlets = new EditText(this);
-    	edtOutlets.setEms(0);
-    	edtOutlets.setTextAppearance(this, android.R.attr.textAppearanceSmall);
-    	edtOutlets.setText(outletno);
-    	edtOutlets.setTag(new Integer(RowCounter));
-    	
-    	ImageButton ibDelete = new ImageButton(this);
-    	ibDelete.setBackgroundResource(android.R.drawable.list_selector_background);
-    	ibDelete.setImageResource(android.R.drawable.ic_delete);
-    	ibDelete.setTag(new Integer(RowCounter));
-    	ibDelete.setOnClickListener(onDeleteClicked);
-    	
-    	TableRow tr = new TableRow(this);
-    	tr.addView(edtName);
-    	tr.addView(edtOutlets);
-    	tr.addView(ibDelete);
-    	tr.setTag(new Integer(RowCounter));
-
-    	tlyoOutlets.addView(tr);
-	}
-	
 	@Override
 	public void onBackPressed() {
-	    
+		boolean allok = true;
+		for (OutletInfo oi: device_info.Outlets) {
+			if (oi.Description == "") {
+				allok = false;
+				break;
+			}
+		}
+		if (! allok) {
+			show_error(getResources().getString(R.string.error_outlet_config_name));
+			return;
+		}
+
+		allok = true;
+		for (OutletInfo oi: device_info.Outlets) {
+			if (oi.OutletNumber < 1) {
+				allok = false;
+				break;
+			}
+		}
+		if (! allok) {
+			show_error(getResources().getString(R.string.error_outlet_config_number));
+			return;
+		}
+		
+		super.onBackPressed();
+	}
+	
+	private void show_error(String message) {
+		AlertDialog aDialog = new AlertDialog.Builder(this)
+			.setMessage(message)
+			.setTitle(getResources().getString(R.string.error_outlet_title))
+			.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					return;
+				}
+			})
+			.create();
+		aDialog.show();
 	}
 	
 	private OnClickListener onBtnAddClicked = new OnClickListener() {
 		public void onClick(View v) {
-			AddRow("", "");
+			device_info.Outlets.add(new OutletInfo());
+			adapter.notifyDataSetChanged();
 		}
 	};
 
 	private OnClickListener onDeleteClicked = new OnClickListener() {
 		public void onClick(View v) {
-			int row_no = (Integer)v.getTag();
-			for (int i=0; i<tlyoOutlets.getChildCount(); i++) {
-				View tr = tlyoOutlets.getChildAt(i);
-				Object tag_o = tr.getTag();
-				if (tag_o != null) {
-					int tr_tag = (Integer)tag_o; 
-					if (tr_tag == row_no) {
-						tlyoOutlets.removeView(tr);
-						break;
-					}
-				}
-			}
+			// TODO Auto-generated method stub
 		}
 	};
 }
---
+++ src/oly/netpowerctrl/OutletConfigAdapter.java
@@ -0,0 +1,74 @@
+package oly.netpowerctrl;
+
+import java.util.List;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+
+public class OutletConfigAdapter extends BaseAdapter {
+
+	private List<OutletInfo> items;
+    private LayoutInflater inflater;
+    
+    public OutletConfigAdapter(Context context, List<OutletInfo> outlets) {
+        inflater = LayoutInflater.from(context);
+        items = outlets;
+    }    
+
+    public int getCount() {
+        return items.size();
+    }
+
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+    	// deliberately a new view because of the textwatchers below
+    	convertView = inflater.inflate(R.layout.outlet_config_item, null);
+
+    	final OutletInfo outlet = items.get(position);
+        EditText tvName = (EditText) convertView.findViewById(R.id.outlet_name);
+        tvName.setText(outlet.Description);
+        tvName.addTextChangedListener(new TextWatcher() {
+			public void onTextChanged(CharSequence s, int start, int before, int count)  {/*nop*/}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after)  {/*nop*/}
+			
+			public void afterTextChanged(Editable s) {
+				outlet.Description = s.toString();
+			}
+		});
+      
+        EditText tvNumber = (EditText) convertView.findViewById(R.id.outlet_number);
+        if (outlet.OutletNumber >= 1) 
+        	tvNumber.setText(((Integer)outlet.OutletNumber).toString());
+        else tvNumber.setText("");
+        tvNumber.addTextChangedListener(new TextWatcher() {
+			public void onTextChanged(CharSequence s, int start, int before, int count)  {/*nop*/}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after)  {/*nop*/}
+			
+			public void afterTextChanged(Editable s) {
+				try {
+					outlet.OutletNumber = Integer.parseInt(s.toString());
+				}
+				catch (Exception e) {
+					outlet.OutletNumber = -1;
+				}
+			}
+		});
+
+        return convertView;
+    }
+
+}
