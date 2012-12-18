904cf0a659c02097e580b84073e9a54bb9cc5135
Oliver Nittka <nittka@nittka.com>
2012-12-18 23:11:49 +0100

    enhance artwork

--- res/raw/make_drawables.sh
+++ res/raw/make_drawables.sh
@@ -9,3 +9,5 @@ convert  -background transparent  widget.svg -resize 36x36 ../drawable-ldpi/widg
 convert  -background transparent  widget.svg -resize 48x48 ../drawable-mdpi/widget.png
 convert  -background transparent  widget.svg -resize 72x72 ../drawable-hdpi/widget.png
 convert  -background transparent  widget.svg -resize 96x96 ../drawable-xhdpi/widget.png
+
+convert  -background transparent  outlet_bg.svg ../drawable/outlet_bg.png
