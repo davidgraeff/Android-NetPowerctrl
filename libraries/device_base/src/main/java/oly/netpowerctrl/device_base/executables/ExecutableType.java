3c7dd56f18fd0027cb985f6a9f20fb948d2a42ff
David Graeff <david.graeff@web.de>
2014-11-06 17:29:05 +0100

    gitignore: Add build and app/build. Remove device_base. Will be a git submodule

--- libraries/device_base/src/main/java/oly/netpowerctrl/device_base/executables/ExecutableType.java
+++
@@ -1,8 +0,0 @@
-package oly.netpowerctrl.device_base.executables;
-
-/**
- * Created by david on 22.10.14.
- */
-public enum ExecutableType {
-    TypeUnknown, TypeRangedValue, TypeToggle, TypeButton
-}
